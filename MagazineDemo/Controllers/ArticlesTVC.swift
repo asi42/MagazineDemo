db4b8d05a1c4a2dc9caadf187ceaa847c86d4463
Asaf h <asaf.haim@collabrium.com>
2019-01-14 21:47:16 +0200

    storyBoard stuff

--- MagazineDemo/Controllers/ArticlesTVC.swift
+++ MagazineDemo/Controllers/ArticlesTVC.swift
@@ -43,10 +43,8 @@ class ArticlesTVC: UITableViewController {
         return 100.0
     }
 
-
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let viewController:ArticlePresentorVC = segue.destination as! ArticlePresentorVC
         if let indexPath = tableView.indexPathForSelectedRow {
